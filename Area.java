24776757c1ed243d3f106a6a6aab298ffacd94f7
souhard <souhardswami987@gmail.com>
2021-08-16 18:09:58 +0530

    completed

---
+++ Area.java
@@ -0,0 +1,55 @@
+package org.neo.neo;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Area {
+
+    public static void main(String[] args) throws IOException {
+        FileInputStream fis = new FileInputStream("input.json");
+        byte[] buffer = new byte[10];
+        StringBuilder sb = new StringBuilder();
+        while (fis.read(buffer) != -1) {
+            sb.append(new String(buffer));
+            buffer = new byte[10];
+        }
+        fis.close();
+
+        String content = sb.toString();
+        Map<String, Figure> response = new ObjectMapper().readValue(content, new TypeReference<Map<String, Figure>>() {});
+        Map<String,String> output = new HashMap<>();
+        for (int i = 1; i < Integer.MAX_VALUE; i++) {
+            Figure val = response.get(""+i);
+            if (val == null) {
+                break;
+            }
+            double area;
+            if (val.type.equals("circle")) {
+                area = round(22.0* val.radius *val.radius /7.0,2);
+                output.put(""+i,""+area);
+            } else if (val.type.equals("rectangle")) {
+                output.put(""+i,""+(val.l*val.b));
+            } else {
+                output.put(""+i,""+(val.side * val.side));
+            }
+
+        }
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(Paths.get("output.json").toFile(), output);
+    }
+    public static double round(double value, int places) {
+        if (places < 0) throw new IllegalArgumentException();
+
+        long factor = (long) Math.pow(10, places);
+        value = value * factor;
+        long tmp = Math.round(value);
+        return (double) tmp / factor;
+    }
+}
---
+++ figure.java
@@ -0,0 +1,81 @@
+package org.neo.neo;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Figure {
+    String type;
+    @JsonIgnoreProperties(ignoreUnknown = true)
+
+    int radius;
+    @JsonIgnoreProperties(ignoreUnknown = true)
+
+    int l;
+    @JsonIgnoreProperties(ignoreUnknown = true)
+
+    int b;
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    int side;
+
+    public Figure() {
+    }
+
+    public Figure(String type, int radius, int l, int b, int side) {
+        this.type = type;
+        this.radius = radius;
+        this.l = l;
+        this.b = b;
+        this.side = side;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    public int getL() {
+        return l;
+    }
+
+    public void setL(int l) {
+        this.l = l;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public int getSide() {
+        return side;
+    }
+
+    public void setSide(int side) {
+        this.side = side;
+    }
+
+    @Override
+    public String toString() {
+        return "Figure{" +
+                "type='" + type + '\'' +
+                ", radius=" + radius +
+                ", l=" + l +
+                ", b=" + b +
+                ", side=" + side +
+                '}';
+    }
+}
